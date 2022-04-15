76e2d9fab2b614198b2bb96dd581ea63e842740a
wantpark <wantpark@gmail.com>
2022-04-15 15:36:23 +0900

    first

---
+++ app/src/main/java/com/iot/api/config.kt
@@ -0,0 +1,9 @@
+package com.iot.api
+
+class Config {
+    val USE_SECURE: Boolean = false
+
+    val MQTT_IP: String = "192.168.0.18"
+    val MQTT_URL: String = "localhost"
+    val MQTT_PORT: Int = 1883
+}
